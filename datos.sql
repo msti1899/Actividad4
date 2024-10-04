cfdf4d18625efe7f800554f9de627551a5476d66
matts1899 <matinacional0@gmail.com>
2024-10-04 14:47:16 -0300

    Datos para sql

--- datos.sql
+++ datos.sql
@@ -1,8 +1,11 @@
-    CREATE TABLE 'registros' (
-        'id' INT NOT NULL AUTO_INCREMENT,
-        'nombre' VARCHAR(50) NOT NULL,
-        'apellido' VARCHAR(50) NOT NULL,
-        'nombre_user' VARCHAR(50) NOT NULL,
-        'email' VARCHAR(100),
-        'numero_telefono' VARCHAR(9),
+    CREATE DATABASE solicitudes;
+    
+    CREATE TABLE registros (
+        id INT NOT NULl Primary key,
+        nombre VARCHAR(50) NOT NULL,
+        apellido VARCHAR(50) NOT NULL,
+        nombre_user VARCHAR(50) NOT NULL,
+        email VARCHAR(100),
+        numero_telefono CHAR(9);
     )
+    
